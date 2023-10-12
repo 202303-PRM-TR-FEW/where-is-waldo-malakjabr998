8d8efe513f072beb6b75850ccb4b2c3d4b977f29
malak jabr <malakjabr2@gmail.com>
2023-10-12 19:33:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -4,5 +4,14 @@ const example = [
   ["A", "B", "A"]];
 
 const whereIsWaldo = (matrix) => {
-  
+  for ( let i=1; i<example.length+1; i++){
+    for (let j=1; j<example[i].length+1; j++){
+      if (example[i][j]==="B"){
+        return [i+1 , j+1 ]
+      }
+    }
+  }
+  return null 
 }
+console.log(whereIsWaldo(example))
+
